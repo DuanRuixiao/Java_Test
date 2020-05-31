da15034c76a0c4fb1554e272075bb24f635d2792
Ruixiao Duan <duanruixiao@gmail.com>
2020-05-31 10:40:34 -0700

    Modified inputs for class Solution.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2};
+        int[] nums = new int[]{4, 2};
 
         Solution solution = new Solution();
         int ans = solution.twoSum(nums);
